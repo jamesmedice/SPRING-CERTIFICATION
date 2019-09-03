7c8863723ebd4d6e5134239c8383d212ff60a5f8
U0I2747 <U0I2747 tiago.medice@gft.com>
2019-09-03 11:55:39 +0200

    jpa spring boot

--- SPRING-JPA-SPRING-DATA/src/main/java/com/medici/app/spring/App.java
+++ SPRING-JPA-SPRING-DATA/src/main/java/com/medici/app/spring/App.java
@@ -3,7 +3,6 @@ package com.medici.app.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * 
@@ -11,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  *
  */
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableJpaAuditing
 public class App {
 
 	public static void main(String[] args) {
