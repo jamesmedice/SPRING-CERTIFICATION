2b9996301284a7c121f75e1f4772620d7b7efb34
U0I2747 <U0I2747 tiago.medice@gft.com>
2019-09-06 16:39:05 +0200

    data jpa test

--- SPRING-JPA-SPRING-DATA/src/test/java/com/medici/app/spring/JPASpecificationsTest.java
+++ SPRING-JPA-SPRING-DATA/src/test/java/com/medici/app/spring/JPASpecificationsTest.java
@@ -13,11 +13,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.medici.app.spring.model.Actor;
 import com.medici.app.spring.repository.ActorRepository;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@EnableEncryptableProperties
 public class JPASpecificationsTest {
 
 	@Autowired
@@ -25,6 +27,7 @@ public class JPASpecificationsTest {
 
 	@Test
 	public void save_Test() {
+
 		Actor entity = new Actor();
 		entity.setFirst_name("John");
 		entity.setLast_name("Irons");
